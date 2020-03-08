292d15b7c5d13fab61617ede0355b35fdd5df718
sihanwang <ocpwj@hotmail.com>
2020-03-08 17:12:19 +0800

    Added 姥姥姥爷

--- SecurityCheck.py
+++ SecurityCheck.py
@@ -1,7 +1,7 @@
 #安检器
 name = input("Hi,你叫什么名字？")
-namelist = [ '王思翰' , '王靖' , '余杨' ]
-if name == namelist[0] or name == namelist[1] or name == namelist[2]:
+namelist = [ '王思翰' , '王靖' , '余杨' , '马俊华' , '余枚魁']
+if name == namelist[0] or name == namelist[1] or name == namelist[2] or  name == namelist[3] or name == namelist[4]:
     print("你好！", name, "，欢迎来到父女节大会！")
 else:
     print( "禁止参加父女节大会，快走开！")
